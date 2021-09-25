6b01be0093d2868011b04535ca02c918e0fe9a7b
krishnatak <68699584+krishnatak@users.noreply.github.com>
2021-09-25 12:44:39 -0700

    Added a chart View

---
+++ src/main/java/com/example/f21comp1011gcw2/CameraSalesViewController.java
@@ -0,0 +1,19 @@
+package com.example.f21comp1011gcw2;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+
+public class CameraSalesViewController {
+
+    @FXML
+    private BarChart<?, ?> barChart;
+
+    @FXML
+    private CategoryAxis cameraAxis;
+
+    @FXML
+    private NumberAxis salesAxis;
+
+}
